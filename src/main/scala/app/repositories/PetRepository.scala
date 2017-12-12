0351d8a1152384cb4f142c41232c92398698b977
Adam Conder <adamdconder@gmail.com>
2017-12-12 14:17:42 +0000

    Change to arrayBuffer

--- src/main/scala/app/repositories/PetRepository.scala
+++ src/main/scala/app/repositories/PetRepository.scala
@@ -2,36 +2,40 @@ package app.repositories
 
 import app.models.{Cat, Dog, Pet}
 
+import scala.collection.mutable.ArrayBuffer
+
 object PetRepository {
 
-  private var _pets : List[Pet] = Nil
+  private val _pets : ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
 
-  def all() : List[Pet] = _pets
+  def all() : List[Pet] = _pets.toList
 
   def findByName(name : String) : Option[Pet] = _pets.find(p => p.name.equalsIgnoreCase(name))
 
   def dogs : List[Dog] = {
-    _pets.collect { case d : Dog => d }
+    _pets.toList.collect { case d : Dog => d }
   }
 
   def cats : List[Cat] = {
-    _pets.flatMap {
+    _pets.toList.flatMap {
       case c @ Cat(_, _) => Some(c)
       case _ => None
     }
   }
 
-  def other : List[Pet] = _pets.filterNot(x => x.isInstanceOf[Cat] || x.isInstanceOf[Dog])
+  def other : List[Pet] = _pets.toList.filterNot(x => x.isInstanceOf[Cat] || x.isInstanceOf[Dog])
 
   def add(pet: Pet*) : List[Pet] = {
-    _pets = _pets ::: pet.toList
+    _pets ++= pet
+
     all()
   }
 
-  def removeByName(name : String) : List[Pet] = _pets.filterNot(p => p.name.equalsIgnoreCase(name))
+  def removeByName(name : String) : List[Pet] = _pets.toList.filterNot(p => p.name.equalsIgnoreCase(name))
 
   def update(original: Pet, modified: Pet) : List[Pet] = {
-    _pets = _pets.updated(indexOfPet(original), modified)
+    _pets.update(indexOfPet(original), modified)
+
     all()
   }
 
